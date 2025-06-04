2dab391b6d64d53f79eedaeb3075485c6e382568
sruthimurali-2 <sruthim03@gmail.com>
2025-06-04 17:52:47 +0530

    home.py changes

---
+++ Home.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from datetime import date
+
+st.set_page_config(page_title="Welcome to IFRS13 Classification model", layout="wide")
+st.sidebar.markdown("""
+### IFRS13 Classification Workflow
+
+Navigate through:
+- **Model Inference**
+- **Risk Factor Testing**
+- **Rationale Explanation**
+
+Use the sidebar to choose a module.
+""")
+
+st.markdown("""
+<style>
+.big-title {
+    font-size: 2.8em;
+    font-weight: 700;
+    color: #0d6efd;
+    margin-bottom: 0.2em;
+    font-family: 'Segoe UI', sans-serif;
+}
+.subtitle {
+    font-size: 1.3em;
+    color: #333;
+    margin-bottom: 1.5em;
+    font-family: 'Segoe UI', sans-serif;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<div class="big-title"> Welcome to the IFRS13 Fair Value Classification Appplication</div>
+<div class="subtitle">A modular, explainable, and friendly classification tool - To be raplced tomo</div>
+""", unsafe_allow_html=True)
+
+st.info("Use the navigation on the left sidebar to begin.")
+
+st.markdown("""
+---
+### 📌 Key Features:
+
+- **Modular Design:** Split workflows for Model Inference, Observability, and Rationale
+- **Regulatory Focus:** Built for IFRS13 compliance scenarios
+- **Explainable AI:** Combines ML models and GPT rationale
+- **Azure-native:** Integrates with Azure ML & OpenAI services
+
+---
+📅 Today's Date: **{}**
+""".format(date.today().strftime("%d %b %Y")))
--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
-streamlit run app.py --server.port=$PORT --server.enableCORS false
+streamlit run Home.py --server.port=$PORT --server.enableCORS false
